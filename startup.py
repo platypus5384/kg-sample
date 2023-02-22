76715c6583329f7a4ea1a7db3032f5b00319e3b3
platypus5384 <51383611+platypus5384@users.noreply.github.com>
2023-02-23 00:49:18 +0900

    2/23

--- startup.py
+++ startup.py
@@ -1,6 +1,21 @@
+import sys
+sys.path.append('C:\\Users\owner\.platformio\penv\lib\site-packages')
+sys.path.append("C:\\Users\owner\appdata\local\packages\pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0\localcache\local-packages\python310\site-packages")
 from flask import Flask, render_template, request
 import json
-import random
+import serial
+
+# import schedule
+
+
+
+# ser = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
+ser = serial.Serial("COM4", 9600)
+
+def read_serial():
+    pass
+
+
 
 app = Flask(__name__)
 
