abcc5843c2ee0384b873fe460708aa3ee27066ef
platypus5384 <csmpp2018@gmail.com>
2023-02-23 21:01:12 +0900

    2/23

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <ArduinoJson.h>
+
 void setup()
 {
     Serial.begin(115200);
@@ -7,5 +9,36 @@ void setup()
 
 void loop()
 {
-    Serial.println("akjlsdjalsd");
+    StaticJsonDocument<1024> doc;
+    doc["IN0"] = 0.000;
+    doc["IN1"] = 0.000;
+    doc["IN2"] = 0.000;
+    doc["IN3"] = 0.000;
+    doc["IN4"] = 0.000;
+    doc["IN5"] = 0.000;
+    doc["IN6"] = 0.000;
+    doc["IN7"] = 0.000;
+    doc["IN8"] = 0.000;
+    doc["IN9"] = 0.000;
+    doc["IN10"] = 0.000;
+    doc["IN11"] = 0.000;
+    doc["OUT0"] = "L";
+    doc["OUT1"] = "L";
+    doc["OUT2"] = "L";
+    doc["OUT3"] = "L";
+    doc["OUT4"] = "L";
+    doc["OUT5"] = "L";
+    doc["OUT6"] = "L";
+    doc["OUT7"] = "L";
+    doc["OUT8"] = "L";
+    doc["OUT9"] = "L";
+    doc["OUT10"] = "L";
+    doc["OUT11"] = "L";
+    String json;
+    serializeJson(doc, json);
+    while (true)
+    {
+        
+        Serial.println(json);
+    }
 }
--- startup.py
+++ startup.py
@@ -5,15 +5,57 @@ from flask import Flask, render_template, request
 import json
 import serial
 
-# import schedule
+import threading
 
+
+pin_data={
+    "IN0": 0.000,
+    "IN1": 0.000,
+    "IN2": 0.000,
+    "IN3": 0.000,
+    "IN4": 0.000,
+    "IN5": 0.000,
+    "IN6": 0.000,
+    "IN7": 0.000,
+    "IN8": 0.000,
+    "IN9": 0.000,
+    "IN10": 0.000,
+    "IN11": 0.000,
+    "OUT0": "L",
+    "OUT1": "L",
+    "OUT2": "L",
+    "OUT3": "L",
+    "OUT4": "L",
+    "OUT5": "L",
+    "OUT6": "L",
+    "OUT7": "L",
+    "OUT8": "L",
+    "OUT9": "L",
+    "OUT10": "L",
+    "OUT11": "L",
+}
 
 
 # ser = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
-ser = serial.Serial("COM4", 9600)
+ser = serial.Serial("/dev/ttyUSB0", 115200)
 
 def read_serial():
-    pass
+    print("---------------------------")
+    json_str = ser.readline().decode("utf-8")
+    try:
+        json_dict = json.loads(json_str)
+        print(json_dict)
+    except json.decoder.JSONDecodeError:
+        print("not json!")
+        
+
+while True:
+    read_serial()
+
+
+
+# thread = threading.Thread(read_serial);
+# thread.start();
 
 
 
