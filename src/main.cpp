84fc7cbf8ff7f35f9f22c379f044f9dd0bff2347
platypus5384 <51383611+platypus5384@users.noreply.github.com>
2023-05-03 06:15:51 +0900

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,49 @@
 
 #include <ArduinoJson.h>
 
-#define PIN_IN1  13
-#define PIN_IN2  14
-#define PIN_IN3  27
-#define PIN_IN4  33
-#define PIN_IN5  32
-#define PIN_IN6  35
-#define PIN_IN7  34
-#define PIN_IN8  39
-#define PIN_IN9  36
+#define PIN_IN1 13
+#define PIN_IN2 14
+#define PIN_IN3 27
+#define PIN_IN4 33
+#define PIN_IN5 32
+#define PIN_IN6 35
+#define PIN_IN7 34
+#define PIN_IN8 39
+#define PIN_IN9 36
 
-#define PIN_OUT1  25
-#define PIN_OUT2  26
-#define PIN_OUT3   2
-#define PIN_OUT4   4
-#define PIN_OUT5  16
-#define PIN_OUT6  17
-#define PIN_OUT7  18
-#define PIN_OUT8  19
-#define PIN_OUT9  21
+#define PIN_OUT1 25
+#define PIN_OUT2 26
+#define PIN_OUT3 2
+#define PIN_OUT4 4
+#define PIN_OUT5 16
+#define PIN_OUT6 17
+#define PIN_OUT7 18
+#define PIN_OUT8 19
+#define PIN_OUT9 21
 #define PIN_OUT10 22
 #define PIN_OUT11 23
 
+volatile uint32_t pulseCount = 0;
 
+void interruptHandler()
+{
+    pulseCount++;
+}
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(PIN_IN1, ANALOG);
-    pinMode(PIN_IN2, ANALOG);
-    pinMode(PIN_IN3, ANALOG);
-    pinMode(PIN_IN4, ANALOG);
-    pinMode(PIN_IN5, ANALOG);
-    pinMode(PIN_IN6, ANALOG);
-    pinMode(PIN_IN7, ANALOG);
-    pinMode(PIN_IN8, ANALOG);
-    pinMode(PIN_IN9, ANALOG);
+    pinMode(PIN_IN2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_IN2), interruptHandler, FALLING);
+    // pinMode(PIN_IN1, ANALOG);
+    // pinMode(PIN_IN2, ANALOG);
+    // pinMode(PIN_IN3, ANALOG);
+    // pinMode(PIN_IN4, ANALOG);
+    // pinMode(PIN_IN5, ANALOG);
+    // pinMode(PIN_IN6, ANALOG);
+    // pinMode(PIN_IN7, ANALOG);
+    // pinMode(PIN_IN8, ANALOG);
+    // pinMode(PIN_IN9, ANALOG);
 
     pinMode(PIN_OUT1, OUTPUT);
     pinMode(PIN_OUT2, OUTPUT);
@@ -77,19 +84,30 @@ void loop()
     doc["OUT11"] = "L";
     String json;
     serializeJson(doc, json);
+    uint32_t b_time_ms = millis();
+    uint32_t time_ms = millis();
+    uint32_t elapse_time_ms;
     while (true)
     {
-        doc["IN1"] = analogRead(PIN_IN1);
-        doc["IN2"] = analogRead(PIN_IN2);
-        doc["IN3"] = analogRead(PIN_IN3);
-        doc["IN4"] = analogRead(PIN_IN4);
-        doc["IN5"] = analogRead(PIN_IN5);
-        doc["IN6"] = analogRead(PIN_IN6);
-        doc["IN7"] = analogRead(PIN_IN7);
-        doc["IN8"] = analogRead(PIN_IN8);
-        doc["IN9"] = analogRead(PIN_IN9);
+        time_ms = millis();
+        elapse_time_ms = time_ms - b_time_ms;
+        if (elapse_time_ms >= 1000)
+        {
+            b_time_ms = time_ms;
+            doc["IN2"] = pulseCount;
+            pulseCount = 0;
+        }
 
-        
+        // doc["IN1"] = analogRead(PIN_IN1);
+        // doc["IN2"] = analogRead(PIN_IN2);
+        // doc["IN3"] = analogRead(PIN_IN3);
+        // doc["IN4"] = analogRead(PIN_IN4);
+        // doc["IN5"] = analogRead(PIN_IN5);
+        // doc["IN6"] = analogRead(PIN_IN6);
+        // doc["IN7"] = analogRead(PIN_IN7);
+        // doc["IN8"] = analogRead(PIN_IN8);
+        // doc["IN9"] = analogRead(PIN_IN9);
+
         digitalWrite(PIN_OUT2, HIGH);
         json.clear();
         serializeJson(doc, json);
